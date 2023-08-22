7bcea8d5c446a0b4ccf6b2aa8969be41555c138b
Dennis Anderson <dennisanderson@Denniss-MacBook-Pro.local>
2023-08-22 15:58:35 -0400

    changed interpolation color

--- src/utils/colorPalette.js
+++ src/utils/colorPalette.js
@@ -1,10 +1,10 @@
-import {scaleSequential, interpolateRdYlBu, interpolateBlues, interpolateOranges, interpolatePurples, bin, extent} from 'd3'; 
+import {scaleSequential, interpolateRdYlBu, interpolateBlues, interpolateOranges, interpolateGreens, bin, extent} from 'd3'; 
 
 function colorPalette(data) {
   // Necessary to convert to numbers from strings
   
   let domain = extent(data); // return min and max
-  let myColor = scaleSequential(interpolateRdYlBu).domain(domain); // generate color scale
+  let myColor = scaleSequential(interpolateGreens).domain(domain); // generate color scale
   let bin1 = bin(); 
   const myBin = bin1(data); // generate bins
   const stops = myBin.map(i => { 
